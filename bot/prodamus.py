be6dc6cba71281f95f985fe9d2fb260856d07f05
Feodor <kologermit@gmail.com>
2024-01-24 13:36:14 +0400

    fix1

--- bot/prodamus.py
+++ bot/prodamus.py
@@ -14,7 +14,8 @@ def prodamus_create_url(product: dict, extra: str, user: types.User, url):
         "_param_user_name": user.full_name,
         "_param_user_username": user.username,
         "_param_user_tg_id": user.id,
-        "_param_product_id": product["id"]
+        "_param_product_id": product["id"],
+        "_params_url_success": url,
     }
     logging.info(f"Prodamus data: {data}")
     for key, value in product.items():
